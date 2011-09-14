e2576e1f2e04826a71ae2725135c2b0b98e65cef
Karol Sanchez <ksanc@folio.sas.upenn.edu>
2011-09-14 15:01:19 -0400

    testing
    just testing the git commands

--- conv.py
+++ conv.py
@@ -3,8 +3,8 @@
 ##module to perform 1D and 2D convolutions
 #definition of a convolution:
 # (f * g)(t)= int(f(tau)G(t-tau) dtau)
-import numpy as n, pylab as p, sys
-
+import numpy as n, pylab as p, scipy as s, sys
+#just testing for git
 f=sys.argv[1]
 g=sys.argv[2]
 
